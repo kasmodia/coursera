830bab6daae2aff100c15dc7a0d2af9fd7fb8b4e
Ahmed Rezk <ahmed.r.badr@gmail.com>
2020-05-31 21:24:49 +0200

    before first submission

--- wordnet/SAP.java
+++ wordnet/SAP.java
@@ -13,24 +13,39 @@ import edu.princeton.cs.algs4.StdOut;
 public class SAP {
 
     private Digraph digraph;
+    private int[] edgeToV;
+    private int[] edgeToW;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
         if (G == null) throw new IllegalArgumentException("Null Digraph");
         this.digraph = new Digraph(G);
+        this.edgeToV = new int[digraph.V()];
+        this.edgeToW = new int[digraph.V()];
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        return 0;
+        return lengthAndAncestor(v, w)[0];
+    }
+
+    private int[] lengthAndAncestor(int v, int w) {
+        int ancestor = ancestor(v, w);
+        if (ancestor == -1) return new int[] {-1, -1};
+        int counter = 0;
+        for (int i = ancestor; i != v; i = edgeToV[i]) {
+            counter++;
+        }
+        for (int i = ancestor; i != w; i = edgeToW[i]) {
+            counter++;
+        }
+        return new int[] {counter, ancestor};
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
         boolean[] vVisited = new boolean[digraph.V()];
         boolean[] wVisited = new boolean[digraph.V()];
-        int[] edgeToV = new int[digraph.V()];
-        int[] edgeToW = new int[digraph.V()];
         Queue<Integer> vQ = new Queue<>();
         vQ.enqueue(v);
         Queue<Integer> wQ = new Queue<>();
@@ -76,26 +91,36 @@ public class SAP {
         return -1;
     }
 
-    private int calcDistance(Integer v, Integer w, Integer intersection, boolean[] vVisited, boolean[] wVisited, int[] edgeToV, int[] edgeToW) {
-        int counter = 0;
-        for (int i = intersection; i != v; i = edgeToV[i]) {
-            counter++;
-        }
-        counter++;
-        for (int i = intersection; i != w; i = edgeToW[i]) {
-            counter++;
-        }
-        return counter;
-    }
-
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+    public int length(Iterable<Integer> vs, Iterable<Integer> ws) {
+        if (vs == null || ws == null) throw new IllegalArgumentException("Null Digraph");
+
+        int[] shortest = new int[] {Integer.MAX_VALUE, Integer.MAX_VALUE};
+        for (Integer v : vs) {
+            for (Integer w : ws) {
+                int[] lengthAndAncestor = lengthAndAncestor(v, w);
+                if (lengthAndAncestor[0] < shortest[0]) {
+                    shortest = lengthAndAncestor;
+                }
+            }
+        }
+        return shortest[0] == Integer.MAX_VALUE ? -1 : shortest[0];
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+    public int ancestor(Iterable<Integer> vs, Iterable<Integer> ws) {
+        if (vs == null || ws == null) throw new IllegalArgumentException("Null Digraph");
+
+        int[] shortest = new int[] {Integer.MAX_VALUE, Integer.MAX_VALUE};
+        for (Integer v : vs) {
+            for (Integer w : ws) {
+                int[] lengthAndAncestor = lengthAndAncestor(v, w);
+                if (lengthAndAncestor[0] < shortest[0]) {
+                    shortest = lengthAndAncestor;
+                }
+            }
+        }
+        return shortest[1] == Integer.MAX_VALUE ? -1 : shortest[1];
     }
 
     // do unit testing of this class
--- wordnet/WordNet.java
+++ wordnet/WordNet.java
@@ -15,14 +15,17 @@ import java.util.Map;
 public class WordNet {
 
     private Map<String, List<Integer>> synsetsMap;
+    private List<String> synsets;
     private Digraph wordNetGraph;
     private int root = -1;
     private SAP sap;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
-        if (synsets == null || hypernyms == null) throw new IllegalArgumentException("Null file names");
+        if (synsets == null || hypernyms == null)
+            throw new IllegalArgumentException("Null file names");
         synsetsMap = new HashMap<>();
+        this.synsets = new ArrayList<>();
 
         initSynsetMap(synsets);
         initWordNetGraph(hypernyms);
@@ -33,10 +36,12 @@ public class WordNet {
         if (in.isEmpty()) throw new IllegalArgumentException("Empty hypernym file");
         String[] lines = in.readAllLines();
         wordNetGraph = new Digraph(lines.length);
-        for (String line: lines) {
+        for (String line : lines) {
             String[] pair = line.split(",");
             if (pair.length == 1) continue;
-            wordNetGraph.addEdge(Integer.parseInt(pair[0]), Integer.parseInt(pair[1]));
+            for (int i = 1; i < pair.length; i++) {
+                wordNetGraph.addEdge(Integer.parseInt(pair[0]), Integer.parseInt(pair[i]));
+            }
         }
         validateGraph();
         sap = new SAP(wordNetGraph);
@@ -55,7 +60,7 @@ public class WordNet {
     private void dfs(int v, boolean[] currentPath, boolean[] visited) {
         visited[v] = true;
         currentPath[v] = true;
-        for (int adj: wordNetGraph.adj(v)) {
+        for (int adj : wordNetGraph.adj(v)) {
             if (currentPath[adj])
                 throw new IllegalArgumentException("Graph is not ascyclic");
             if (!visited[adj])
@@ -77,13 +82,15 @@ public class WordNet {
             String line = in.readLine();
             String[] splits = line.split(",");
             int id = Integer.parseInt(splits[0]);
+            this.synsets.add(splits[1]);
             // split nounds in the same synset
-            for (String noun: splits[1].split(" ")) {
+            for (String noun : splits[1].split(" ")) {
                 // construct a map with noun as key and ID as value
                 // if a noun appears in more than one synset, add it to the list of IDs
                 if (synsetsMap.containsKey(noun)) {
                     synsetsMap.get(noun).add(id);
-                } else {
+                }
+                else {
                     List<Integer> list = new ArrayList<>();
                     list.add(id);
                     synsetsMap.put(noun, list);
@@ -114,13 +121,32 @@ public class WordNet {
     public String sap(String nounA, String nounB) {
         if (!isNoun(nounA) || !isNoun(nounB))
             throw new IllegalArgumentException("Nouns must exist in WordNet");
-        // int ancestor = this.sap.ancestor(synsetsMap.get(nounA), synsetsMap.get(nounB));
-        // wordNetGraph.shortestPath();
-        return null;
+        int ancestor = sap.ancestor(synsetsMap.get(nounA), synsetsMap.get(nounB));
+        return synsets.get(ancestor);
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
         WordNet wordNet = new WordNet("synsets.txt", "hypernyms.txt");
+        System.out.println("worm is a noun: " + wordNet.isNoun("worm"));
+        System.out.println("kasmodia is a noun: " + wordNet.isNoun("kasmodia"));
+        System.out.println("'null' is a noun: " + wordNet.isNoun(null));
+
+        System.out.println("distance 'white_marlin' and 'mileage': "
+                                   + wordNet.distance("white_marlin", "mileage"));
+
+        System.out.println("distance 'Black_Plague' and 'black_marlin': "
+                                   + wordNet.distance("Black_Plague", "black_marlin"));
+
+        System.out.println("distance 'American_water_spaniel' and 'histology': "
+                                   + wordNet.distance("American_water_spaniel", "histology"));
+
+        System.out.println("distance 'Brown_Swiss' and 'barrel_roll': "
+                                   + wordNet.distance("Brown_Swiss", "barrel_roll"));
+
+        System.out.println("SCA between 'individual' and 'edible_fruit': "
+                                   + wordNet.sap("individual", "edible_fruit"));
+        System.out.println("distance between 'individual' and 'edible_fruit': "
+                                   + wordNet.distance("individual", "edible_fruit"));
     }
 }
