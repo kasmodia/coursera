40a1a37a79ead62c1fc376c84b5547ec7b0b1161
Ahmed Rezk <ahmed.r.badr@gmail.com>
2020-05-31 21:35:15 +0200

    outcast implementation

---
+++ wordnet/Outcast.java
@@ -0,0 +1,41 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Outcast {
+
+    private final WordNet wordnet;
+
+    // constructor takes a WordNet object
+    public Outcast(WordNet wordnet) {
+        this.wordnet = wordnet;
+    }
+
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns) {
+        String outcast = null;
+        int max = Integer.MIN_VALUE;
+        for (String nounA: nouns) {
+            int nounAmax = Integer.MIN_VALUE;
+
+            for (String nounB: nouns) {
+                nounAmax += wordnet.distance(nounA, nounB);
+            }
+
+            if (nounAmax > max) {
+                max = nounAmax;
+                outcast = nounA;
+            }
+        }
+        return outcast;
+    }
+
+    public static void main(String[] args) {
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+        }
+    }
+}
